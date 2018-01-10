8fdd8f91acfc76bfc37d4c7cfd28178d579cecd8
Tu Luong <tluong@volute.co>
2018-01-09 22:43:58 -0500

    Update

--- php/downloadData.php
+++ php/downloadData.php
@@ -3,12 +3,8 @@
   header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
   header('Access-Control-Allow-Methods: GET, POST, PUT');
 
-  // PHP libraries for PHP 5.1.5
-  // include_once('php-lib/pclzip/pclzip.lib.php');
-
   $download = false;
   $filename = '../data/data-' . date('Y-m-d') . '.zip';
-  // $files = array('../data');
 
   // Get real path for our folder
   $rootPath = realpath('../data');
@@ -20,6 +16,7 @@
   // Create recursive directory iterator
   /** @var SplFileInfo[] $files */
   $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($rootPath), RecursiveIteratorIterator::LEAVES_ONLY);
+  $exclude_types = array('zip');
 
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // get input data
@@ -29,26 +26,21 @@
     $accounts = json_decode(file_get_contents('../data/accounts.json'));
     foreach ($accounts as $key => $acc) {
       if ($account->id == $acc->id && $account->role == 'admin') {
-        array_map('unlink', glob("../data/*.zip"));
         foreach ($files as $name => $file) {
-          // Skip directories
-          if (!$file->isDir()) {
-            // Get real and relative path for current file
-            $filePath = $file->getRealPath();
-            $relativePath = substr($filePath, strlen($rootPath) + 1);
-            // Add current file to archive
-            $zip->addFile($filePath, $relativePath);
+          $filetype = pathinfo($file, PATHINFO_EXTENSION);
+          if (!in_array(strtolower($filetype), $exclude_types)) {
+            // Skip directories
+            if (!$file->isDir()) {
+              // Get real and relative path for current file
+              $filePath = $file->getRealPath();
+              $relativePath = substr($filePath, strlen($rootPath) + 1);
+              // Add current file to archive
+              $zip->addFile($filePath, $relativePath);
+            }
           }
         }
         // Zip archive will be created only after closing object
         $zip->close();
-        // $archive = new PclZip($filename);
-        // $list = $archive->create($files);
-        // if ($list == 0) {
-        //   die("Error: ".$archive->errorInfo(true));
-        // } else {
-        //   $download = true;
-        // }
         $download = true;
       }
     }
